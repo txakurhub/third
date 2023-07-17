82428fbc9956ae8e672ef7cf4b20e0d919d8c5eb
unknown <me.leandrop@gmail.com>
2023-07-17 11:25:11 -0300

    progressbar

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const ProgressBar = () => {
+  const [scrollProgress, setScrollProgress] = useState(100);
+
+  const handleScroll = () => {
+    const totalScroll = document.documentElement.scrollTop;
+    const windowHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const progress = `${0 + (totalScroll / windowHeight) * 100}%`;
+    setScrollProgress(progress);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-1 bg-white">
+      <div className="h-full bg-orange" style={{ width: scrollProgress }}></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
